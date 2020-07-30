523940de16b65d1ee02f50a42b18e0384834a33c
sh3rLock3d <alipourghasemi78@gmail.com>
2020-07-31 00:12:59 +0430

    server extends thread now and handles several Client some command added for Client

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,58 +1,77 @@
 package client;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.InetAddress;
+import java.io.*;
 import java.net.Socket;
-import java.net.URLEncoder;
+import java.util.Formatter;
+import java.util.Scanner;
 
 public class Client {
     private static final int PORT = 8080;
 
-    public static void main(String[] args) {
-
-        try {
-
-            String params = URLEncoder.encode("param1", "UTF-8")
-                    + "=" + URLEncoder.encode("value1", "UTF-8");
-            params += "&" + URLEncoder.encode("param2", "UTF-8")
-                    + "=" + URLEncoder.encode("value2", "UTF-8");
-
-            String hostname = "127.0.0.1";
-
-            InetAddress addr = InetAddress.getByName(hostname);
-            Socket socket = new Socket(addr, PORT);
-            String path = "/myapp";
-
-            // Send headers
-            BufferedWriter wr =
-                    new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF8"));
-            wr.write("POST "+path+" HTTP/1.0\r\n");
-            wr.write("Content-Length: "+params.length()+"\r\n");
-            wr.write("Content-Type: application/x-www-form-urlencodedrn");
-            wr.write("\r\n");
-
-            // Send parameters
-            wr.write(params);
-            wr.flush();
-
-            // Get response
-            BufferedReader rd = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String line;
-
-            while ((line = rd.readLine()) != null) {
-                System.out.println(line);
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        Formatter formatter = null;
+        Scanner scanner1 = null;
+        Socket socket = null;
+        while (true){
+            String l = scanner.nextLine();
+            if (l.equals("send r 1")) {
+                request1(scanner1, formatter);
+            } else if (l.equals("send r 2")) {
+                request2(scanner1, formatter);
+            } else if (l.equals("start")) {
+                socket = new Socket("127.0.0.1", 8080);
+                scanner1 = new Scanner(socket.getInputStream());
+                formatter = new Formatter(socket.getOutputStream());
+            } else if (l.equals("end")) {
+                socket.close();
             }
-
-            wr.close();
-            rd.close();
-
         }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+    }
 
+    public static void request1(Scanner scanner, Formatter formatter){
+
+        String req = "GET / HTTP/1.1\n" +
+                "Connection: keep-alive\n" +
+                "Keep-Alive: 60\n" +
+                "Accept-Encoding: gzip\n";
+
+        formatter.format(req);
+        formatter.flush();
+        formatter.format("\n");
+        formatter.flush();
+        String line = scanner.nextLine();
+        while (!line.isEmpty()){
+            System.out.println(line);
+            line = scanner.nextLine();
+        }
+        line = scanner.nextLine();
+        while (!line.isEmpty()){
+            System.out.println(line);
+            line = scanner.nextLine();
+        }
+        System.out.println("..........");
+    }
+
+    public static void request2(Scanner scanner, Formatter formatter){
+        String req = "GET /first.html HTTP/1.1\n" +
+                "Connection: keep-alive\n" +
+                "Keep-Alive: 60\n" +
+                "Accept-Encoding: gzip\n";
+        formatter.format(req);
+        formatter.flush();
+        formatter.format("\n");
+        formatter.flush();
+        String line = scanner.nextLine();
+        while (!line.isEmpty()){
+            System.out.println(line);
+            line = scanner.nextLine();
+        }
+        line = scanner.nextLine();
+        while (!line.isEmpty()){
+            System.out.println(line);
+            line = scanner.nextLine();
+        }
+        System.out.println("..........");
     }
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -3,50 +3,46 @@ package server;
 import util.HTTP_request;
 import util.HTTP_response;
 
-import java.net.*;
 import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-
-public class Server {
+public class Server extends Thread{
     private static final int PORT = 8080;
-    private boolean close = false;
+    private Socket client;
 
-    private static String getRes(String path) throws FileNotFoundException {
-        File file=new File(path);
-        FileInputStream fileInputStream=new FileInputStream(file);
-        byte[] data=new byte[(int) file.length()];
-        BufferedInputStream bufferedInputStream=new BufferedInputStream(fileInputStream);
+    public Server(Socket client){
+        this.client = client;
+    }
+
+    @Override
+    public void run() {
         try {
-            bufferedInputStream.read(data,0,data.length);
+            runServer();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return new String(data);
     }
 
-    public static void main(String [] args) throws IOException {
-        ServerSocket server = new ServerSocket(PORT);
-        while (true){
-            Socket client = server.accept();
-            // 1. Read HTTP request from the client socket
-            InputStreamReader isr = new InputStreamReader(client.getInputStream());
-            BufferedReader reader = new BufferedReader(isr);
-            String line = reader.readLine();
-            String HTTPRequest = line;
-            while (!line.isEmpty()){
-                HTTPRequest += line + '\n';
-                line = reader.readLine();
-            }
-
-            // 2. Prepare an HTTP response
-            String HTTPResponse = handleClientRequest(HTTPRequest);
-            // 3. Send HTTP response to the client
-            client.getOutputStream().write(HTTPResponse.getBytes("UTF-8"));
-            // 4. Close the socket
-            client.close();
+    private void runServer() throws IOException {
+        // 1. Read HTTP request from the client socket
+        InputStreamReader isr = new InputStreamReader(client.getInputStream());
+        BufferedReader reader = new BufferedReader(isr);
+        String line = reader.readLine();
+        String HTTPRequest = line;
+        while (!line.isEmpty()){
+            HTTPRequest += line + '\n';
+            line = reader.readLine();
         }
+        System.out.println("here...");
+        // 2. Prepare an HTTP response
+        String HTTPResponse = handleClientRequest(HTTPRequest);
+        // 3. Send HTTP response to the client
+        client.getOutputStream().write(HTTPResponse.getBytes("UTF-8"));
+        // 4. Close the socket
+        client.close();
     }
 
     private static String handleClientRequest(String httpRequest) {
@@ -83,5 +79,26 @@ public class Server {
         return HTTP_response.OK_response(res, HTTP_response.findType(url));
     }
 
-}
+    private static String getRes(String path) throws FileNotFoundException {
+        File file=new File(path);
+        FileInputStream fileInputStream=new FileInputStream(file);
+        byte[] data=new byte[(int) file.length()];
+        BufferedInputStream bufferedInputStream=new BufferedInputStream(fileInputStream);
+        try {
+            bufferedInputStream.read(data,0,data.length);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new String(data);
+    }
 
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(PORT);
+        while (true) {
+            System.out.println("waiting...");
+            Socket client = server.accept();
+            Thread t = new Server(client);
+            t.start();
+        }
+    }
+}
